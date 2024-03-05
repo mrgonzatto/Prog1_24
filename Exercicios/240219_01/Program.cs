ca6431dd115a520a3e691ddf1be5f6f89eb1aee5
Mauricio Gonzatto <mauriciogonzatto@hotmail.com>
2024-03-04 21:08:55 -0300

    Tipos numéricos

--- Exercicios/240219_01/Program.cs
+++ Exercicios/240219_01/Program.cs
@@ -49,3 +49,23 @@ bool isNomeEqual2 =
 Console.WriteLine($"Primeiro resultado: {isNomeEqual}");
 Console.WriteLine($"Segundo resultado: {isNomeEqual2}");
 
+// TIPOS NUMÉRICOS
+/*
+    sbyte: Armazena valores entre -128 e 127
+    short: Armazena valores entre 
+        -32,768 e 32,767
+    int:   Armazena valores entre 
+        -2,147,483,648 e 
+        2,147,483,647
+    long:  Armazena valores entre 
+        -9,223,372,036,854,775,808 e 
+        -9,223,372,036,854,775,807 
+*/
+
+float myFloat = 10.0f;
+double myDouble = 5d;
+decimal myDecimal = 7m;
+int myInteger = 0;
+
+
+
