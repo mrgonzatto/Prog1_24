7c2dfd8243577e905b39b975b3d4e673d5ce8357
Mauricio Gonzatto <mauriciogonzatto@hotmail.com>
2024-04-22 19:30:52 -0300

    Patter Repository

---
+++ Exercicios/240325_01/Program.cs
@@ -0,0 +1,21 @@
+﻿using _240325_01.Models;
+
+// Primeira Clinica
+Clinica c1 = new Clinica();
+Clinica.InstanceCount = 1;
+c1.ObjectCount = 1;
+
+// Segunda Clinica
+Clinica c2 = new Clinica();
+Clinica.InstanceCount++;
+c2.ObjectCount = 10;
+
+Console.WriteLine(
+    $"Valor C1: {c1.ObjectCount}"
+);
+Console.WriteLine(
+$"Instance Count: {Clinica.InstanceCount}"
+);
+Console.WriteLine(
+    $"Valor C2: {c2.ObjectCount}"
+);
---
+++ Exercicios/240401_01/Program.cs
@@ -0,0 +1,21 @@
+﻿using _240401_01.Models;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Customer c1 = new Customer();
+c1.CustomerId = 1;
+c1.Name = "Jaguara";
+c1.EmailAddress = "jaguara@pixote.net";
+
+// Construtor de Instanciação
+Customer c2 = new Customer(2);
+c1.Name = "Boca-Mole";
+c1.EmailAddress = "bocamole@pixote.net";
+
+// Construtor por atribuição
+Customer c3 = new Customer{
+    CustomerId = 3,
+    Name = "Nerso",
+    EmailAddress = "nerso@pixote.com"
+};
